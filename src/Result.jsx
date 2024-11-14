656fdd4e96b39a4510501def859fad89649795b1
sup14e <luckymackie7794@gmail.com>
2024-11-14 09:56:43 +0700

    Added Result page

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,10 +1,44 @@
 import "./Result.css";
+import "./Char.css";
+import { useState } from "react";
+import logo from "./assets/picture/logo.png"
 
 function Result() {
+    // const [currentPage, setcurrentPage] = useState(1)
+    // const [postPerPage, setpostPerPage] = useState(10)
+    
+    // const lastPostIndex = currentPage * postPerPage;
+    // const firstPostIndex = lastPostIndex - postPerPage;
+    // const currentPost = coinsData.slice(firstPostIndex,lastPostIndex);
+
     return (<>
-     <div className="BG">
-        <p>HOWDY!</p>
-    </div>
+        <div className="BG">
+            <div className="navbar">
+                <a href="/"><img src={logo} alt="Logo"/></a>
+                <div className="search-nav">
+                    <input type="text" placeholder="Search" className="search-bar"/>
+                </div>
+            </div>
+
+            <h1 className="return">Characters Found: #</h1>
+
+            <div className="charList">
+                {/* first row */}
+                <a href="/character"><img/> <p>Name</p></a>
+                <a href="/character"><img/> <p>Name</p></a>
+                <a href="/character"><img/> <p>Name</p></a>
+                <a href="/character"><img/> <p>Name</p></a>
+                <a href="/character"><img/> <p>Name</p></a>
+                {/* second row */}
+                <a href="/character"><img/> <p>Name</p></a>
+                <a href="/character"><img/> <p>Name</p></a>
+                <a href="/character"><img/> <p>Name</p></a>
+                <a href="/character"><img/> <p>Name</p></a>
+                <a href="/character"><img/> <p>Name</p></a>
+            </div>
+
+
+        </div>
     </>)
 }
 
